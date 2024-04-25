afc347403c63121ff3504ce3ba33a5fb86fa15b8
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-04-25 10:59:03 +0900

    Add: Read All Items function

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ app.post("/item/create", async (req, res) => {
 });
 
 // Read All Items
+app.get("/", async (req, res) => {
+  try {
+    await connectDB();
+    const allItems = await ItemModel.find();
+    return res
+      .status(200)
+      .json({ message: "アイテム読み取り成功（オール）", allItems: allItems });
+  } catch (err) {
+    return res.status(400).json({ message: "アイテム読み取り失敗（オール）" });
+  }
+});
 // Read single Item
 // Update Item
 // Delete Item
