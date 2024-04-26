0afa7c66d219376b3d7b08f3ee654cda5d9b2f12
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-04-26 09:30:17 +0900

    Add: Complete Read Single Item function

--- index.js
+++ index.js
@@ -30,9 +30,20 @@ app.get("/", async (req, res) => {
     return res.status(400).json({ message: "アイテム読み取り失敗（オール）" });
   }
 });
-// Read single Item
-app.get("/item/:id", (req, res) => {
-  return res.status(200).json({ message: "アイテム読み取り成功（シングル）" });
+// Read Single Item
+app.get("/item/:id", async (req, res) => {
+  try {
+    await connectDB();
+    const singleItem = await ItemModel.findById(req.params.id);
+    return res.status(200).json({
+      message: "アイテム読み取り成功（シングル）",
+      singleItem: singleItem,
+    });
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ message: "アイテム読み取り失敗（シングル）" });
+  }
 });
 // Update Item
 // Delete Item
