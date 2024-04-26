a194fccf815a4bbd81bc27408257a3aa86745b77
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-04-26 09:50:47 +0900

    Add: Delete Item function

--- index.js
+++ index.js
@@ -56,6 +56,15 @@ app.put("/item/update/:id", async (req, res) => {
   }
 });
 // Delete Item
+app.delete("/item/delete/:id", async (req, res) => {
+  try {
+    await connectDB();
+    const r = await ItemModel.deleteOne({ _id: req.params.id });
+    return res.status(200).json({ message: "アイテム削除成功", r: r });
+  } catch (error) {
+    return res.status(400).json({ message: "アイテム削除失敗" });
+  }
+});
 
 // USER functions
 // Register User
