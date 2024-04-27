1c8a37c37928081fd107d9ce7e0c43f0c33d6acc
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-04-27 11:54:13 +0900

    Add: Check password

--- index.js
+++ index.js
@@ -83,7 +83,13 @@ app.post("/user/login", async (req, res) => {
     await connectDB();
     const savedUserData = await UserModel.findOne({ email: req.body.email });
     if (savedUserData) {
-      return res.status(200).json({ message: "ログイン成功" });
+      if (req.body.password === savedUserData.password) {
+        return res.status(200).json({ message: "ログイン成功" });
+      } else {
+        return res
+          .status(400)
+          .json({ message: "ログイン失敗：パスワードが間違っています" });
+      }
     } else {
       return res
         .status(400)
